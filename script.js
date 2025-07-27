a00768d2133bc9a4bf8ce0e5ffaf8d4593981483
Neha Kumarii 123 <nknehakumariii810@gmail.com>
2025-07-27 19:06:05 +0500

    Implemented user profile dropdown with CSS transition

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const authModal = document.querySelector('.auth-modal');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
-const loginBtn = document.querySelector('.login-btn-modal');
+const loginBtnModal = document.querySelector('.login-btn-modal');
 const closeBtnModal = document.querySelector('.close-btn-modal');
+const profileBox = document.querySelector('.profile-box');
+const avatarCircle = document.querySelector('.avatar-circle');
+
+
 
 registerLink.addEventListener('click', () => authModal.classList.add('slide'));
 loginLink.addEventListener('click', () => authModal.classList.remove('slide'));
-loginBtn.addEventListener('click', () => authModal.classList.add('show'));
+loginBtnModal.addEventListener('click', () => authModal.classList.add('show'));
 closeBtnModal.addEventListener('click', () => authModal.classList.remove('show' , 'slide'));
+
+avatarCircle.addEventListener('click', () => profileBox.classList.toggle('show'));
